be14ac524a04a6e2833716f53c5fda587bb5149a
Gazi Akter <gaziakterdev@gmail.com>
2024-10-10 16:27:12 +0600

    add writer

--- app/Http/Controllers/WriterControlller.php
+++ app/Http/Controllers/WriterControlller.php
@@ -18,4 +18,46 @@ class WriterControlller extends Controller
         $data['getCategory'] = WriterModel::getRecord();
         return view('panel.writer.add', $data);
     } 
+
+        // Insert Write
+        public function insert(Request $request)
+        {
+       
+            // Validate the request data
+            $request->validate([
+                'name' => 'required|string|unique:writers,name', // Ensure the category name is unique
+                'address' => 'nullable|string|max:500',
+                'email' => 'nullable|email|max:255|unique:writers,email',
+                'mobile' => 'nullable|numeric|digits_between:10,15|unique:writers,mobile',
+                'website' => 'nullable|url|max:255',
+                'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Optional, must be an image file
+            ]);
+           
+            // Create a new instance of the CategoryModel for insertion
+            $writerData = new WriterModel();
+        
+            // Set the category details
+            $writerData->name = $request->name;
+            $writerData->address = $request->address;
+            $writerData->mobile = $request->mobile;
+            $writerData->email = $request->email;
+            $writerData->website = $request->website;
+
+            $writerData->slug = strtolower(str_replace(' ', '-', $request->name));
+
+                    // Handle file upload if the logo is present
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            $filename = hexdec(uniqid()).'.'.$file->getClientOriginalExtension();  // 3434343443.jpg
+            $file->move(public_path('assets/upload/writer'), $filename);
+            $writerData->photo = $filename;
+        }
+    
+            // Save the new category record
+            if ($writerData->save()) {
+                return redirect('panel/writer')->with('success', 'Writer Successfully Created');
+            } else {
+                return redirect()->back()->with('error', 'Failed to add Writer');
+            }
+        }
 }
--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::group(['middleware' => 'basicuser'], function(){
     // Writer Route
     Route::get('panel/writer', [WriterControlller::class, 'list']);
     Route::get('panel/writer/add', [WriterControlller::class, 'add']);
-    // Route::post('panel/writer/add', [WriterControlller::class, 'insert']);
+    Route::post('panel/writer/add', [WriterControlller::class, 'insert']);
     // Route::get('panel/writer/edit/{id}', [WriterControlller::class, 'edit']);
     // Route::post('panel/writer/edit/{id}', [WriterControlller::class, 'update']);
     // Route::get('panel/writer/delete/{id}', [WriterControlller::class, 'delete']);
