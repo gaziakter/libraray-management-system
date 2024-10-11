e60e35cbee7e3d035bcd7d87fddfc0605d469b6a
Gazi Akter <gaziakterdev@gmail.com>
2024-10-11 07:06:09 +0600

    add book function

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\BookModel;
+use App\Models\WriterModel;
+use App\Models\PublisherModel;
+use App\Models\CagegoryModel;
+use App\Models\SubCategoryModel;
 
 class BookController extends Controller
 {
@@ -12,4 +16,13 @@ class BookController extends Controller
         $data['getRecord'] = BookModel::getRecord();
         return view('panel.book.list',  $data);
     }
+
+        // add 
+        public function add(){
+            $data['getWriter'] = WriterModel::getRecord();
+            $data['getPublisher'] = PublisherModel::getRecord();
+            $data['getCagegory'] = CagegoryModel::getRecord();
+            $data['getSubCategory'] = SubCategoryModel::getRecord();
+            return view('panel.book.add', $data);
+        } 
 }
