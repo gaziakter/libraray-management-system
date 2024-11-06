5c0a6c846c20f18993f7e209eb55feaf9099e4d7
Gazi Akter <gaziakterdev@gmail.com>
2024-11-06 21:48:37 +0600

    show liseted items

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\AuthorModel;
+use App\Models\BookModel;
+use App\Models\StudentModel;
+use App\Models\PublisherModel;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,11 +13,12 @@ class DashboardController extends Controller
     //
         //
         public function dashboard(){
+            $book = BookModel::all()->count();
+            $student = StudentModel::all()->count();
+            $author = AuthorModel::all()->count();
+            $publisher = PublisherModel::all()->count();
 
-            $getAuthor = AuthorModel::all();
-            $author = $getAuthor->count();
-
-            return view('panel.dashboard', compact('author'));
+            return view('panel.dashboard', compact('book', 'student', 'author', 'publisher'));
     
         }
 }
--- resources/views/panel/dashboard.blade.php
+++ resources/views/panel/dashboard.blade.php
@@ -12,7 +12,38 @@
           <div class="card">
             <div class="card-body text-center pt-5 pb-5">
               <div class="dashboard-box pt-4 pb-4">
-                <h2>{{$author}} Authors Listed <a href=""class="badge bg-info mt-3">Show Details</a></h2>
+                <h2>{{$book}} Books Listed</h2>
+                <h3><a href="{{url('panel/book')}}"class="badge bg-info mt-3">Show Details</a></h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card">
+            <div class="card-body text-center pt-5 pb-5">
+              <div class="dashboard-box pt-4 pb-4">
+                <h2>{{$student}} Students Registed</h2>
+                <h3><a href="{{url('panel/student')}}"class="badge bg-info mt-3">Show Details</a></h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card">
+            <div class="card-body text-center pt-5 pb-5">
+              <div class="dashboard-box pt-4 pb-4">
+                <h2>{{$author}} Authors Listed</h2>
+                <h3><a href="{{url('panel/author')}}"class="badge bg-info mt-3">Show Details</a></h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card">
+            <div class="card-body text-center pt-5 pb-5">
+              <div class="dashboard-box pt-4 pb-4">
+                <h2>{{$publisher}} Publishers Listed</h2>
+                <h3><a href="{{url('panel/publisher')}}"class="badge bg-info mt-3">Show Details</a></h3>
               </div>
             </div>
           </div>
