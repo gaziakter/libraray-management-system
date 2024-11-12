8ed5ed7945a77072053bc64383fb0c700ec2633b
Gazi Akter <gaziakterdev@gmail.com>
2024-11-12 06:47:00 +0600

    insert model

--- app/Models/PermissionRoleModel.php
+++ app/Models/PermissionRoleModel.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class PermissionRoleModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'role_permission';
+
+
+    static public function getRolePermission($role_id){
+
+        return PermissionRoleModel::where('role_id', '=', $role_id)->get();
+
+    }
+
+    static public function getPermission($slug, $role_id){
+
+        return PermissionRoleModel::select('permission_role.id')
+        ->join('permission', 'permission.id', '=', 'permission_role.permission_id')
+        ->where('permission_role.role_id', '=', $role_id)
+        ->where('permission.slug', '=', $slug)
+        ->count();
+
+    }
 }
